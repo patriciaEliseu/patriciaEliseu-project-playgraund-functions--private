18e38d82801b3ec2bbc49e07590aa4d89b9653d6
Patricia Eliseu <patyeliseu4@gmail.com>
2022-02-09 16:00:05 -0300

    adicionei desafio 4

--- src/challenges.js
+++ src/challenges.js
@@ -23,54 +23,58 @@ function splitSentence(string) {
 
 
 // Desafio 4
-function concatName() {
-    // seu código aqui
-}
+function concatName(string) {
+    let a = concatName.length - 1;
+    let b = concatName(0);
 
-// Desafio 5
-function footballPoints() {
-    // seu código aqui
-}
 
-// Desafio 6
-function highestCount() {
-    // seu código aqui
-}
+    return (b, a);
 
-// Desafio 7
-function catAndMouse() {
-    // seu código aqui
-}
 
-// Desafio 8
-function fizzBuzz() {
-    // seu código aqui
-}
+    // Desafio 5
+    function footballPoints() {
+        // seu código aqui
+    }
 
-// Desafio 9
-function encode() {
-    // seu código aqui
-}
+    // Desafio 6
+    function highestCount() {
+        // seu código aqui
+    }
 
-function decode() {
-    // seu código aqui
-}
+    // Desafio 7
+    function catAndMouse() {
+        // seu código aqui
+    }
 
-// Desafio 10
-function techList() {
-    // seu código aqui
-}
+    // Desafio 8
+    function fizzBuzz() {
+        // seu código aqui
+    }
 
-module.exports = {
-    calcArea,
-    catAndMouse,
-    compareTrue,
-    concatName,
-    decode,
-    encode,
-    fizzBuzz,
-    footballPoints,
-    highestCount,
-    splitSentence,
-    techList,
-};
+    // Desafio 9
+    function encode() {
+        // seu código aqui
+    }
+
+    function decode() {
+        // seu código aqui
+    }
+
+    // Desafio 10
+    function techList() {
+        // seu código aqui
+    }
+
+    module.exports = {
+        calcArea,
+        catAndMouse,
+        compareTrue,
+        concatName,
+        decode,
+        encode,
+        fizzBuzz,
+        footballPoints,
+        highestCount,
+        splitSentence,
+        techList,
+    };
